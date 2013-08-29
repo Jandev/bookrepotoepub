83fae68a8219e3a5509f5fe7fbeffc5890f5a8c2
Jan de Vries <jan@jan-v.nl>
2013-08-29 19:51:40 +0200

    Oranized usings, removed break for final build

--- bookrepotoepub.console/Program.cs
+++ bookrepotoepub.console/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace bookrepotoepub.console
 {
@@ -23,7 +20,7 @@ namespace bookrepotoepub.console
                 extracter.ExtractArchiveToLocalPath(archive);
                 Console.WriteLine("Extracted: " + archive);
 #if DEBUG
-                break;
+                //break;
 #endif
             }
             Console.WriteLine("**********************");
